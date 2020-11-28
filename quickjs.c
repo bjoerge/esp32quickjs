90450cb34439144bf65ed29f0bb231bded2ca237
Kousuke Kawahira <kawahira@binzume.net>
2020-11-28 22:18:48 +0900

    reduce firmware size

--- quickjs.c
+++ quickjs.c
@@ -49,7 +49,7 @@
 
 #define OPTIMIZE         1
 #define SHORT_OPCODES    1
-#if defined(EMSCRIPTEN)
+#if defined(EMSCRIPTEN) || defined(ESP32)
 #define DIRECT_DISPATCH  0
 #else
 #define DIRECT_DISPATCH  1
@@ -68,7 +68,7 @@
 
 /* define to include Atomics.* operations which depend on the OS
    threads */
-#if !defined(EMSCRIPTEN)
+#if !defined(EMSCRIPTEN) && !defined(ESP32)
 #define CONFIG_ATOMICS
 #endif
 
